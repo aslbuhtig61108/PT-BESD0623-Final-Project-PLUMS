be2443097a27248dd0d95c3e73ceae1c5bcfaa9d
Ed Locquiao <nilatian.15@gmail.com>
2022-11-17 00:35:53 -0800

    Fourth commit: throw away test successfully counted 22 rows of data from books table. This means it counted all the rows in the books table that was created by the application.

--- plums-final-project/src/test/java/com/promineotech/plums/controller/BookEntityCrudTest.java
+++ plums-final-project/src/test/java/com/promineotech/plums/controller/BookEntityCrudTest.java
@@ -6,16 +6,18 @@ import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
-
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.plums.controller.support.BookEntityCrudTestSupport;
 import com.promineotech.plums.entity.Book;
 import com.promineotech.plums.entity.Genre;
@@ -26,11 +28,14 @@ import com.promineotech.plums.entity.Genre;
 		"classpath:flyway/migrations/V1.1__Plums_Data.sql" },
 		config = @SqlConfig(encoding = "utf-8"))
 class BookEntityCrudTest extends BookEntityCrudTestSupport {
-
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@Test
 	void testDb() {
-		
+		int numberOfRows = JdbcTestUtils.countRowsInTable(jdbcTemplate, "books");
+		System.out.println("Number of Row =" + numberOfRows);
 	}
 	
 	@Disabled
